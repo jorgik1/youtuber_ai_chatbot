c3daa64e9566295b31a3e574037e08bc8e6f92a3
Yurii Stenin <yuriy.s@team.anyforsoft.com>
2023-05-23 15:49:50 +0300

    Changed regex expression.

--- youtuber_ai_chatbot/components/youtube.py
+++ youtuber_ai_chatbot/components/youtube.py
@@ -4,33 +4,6 @@ from langchain.docstore.document import Document
 from typing import List, Any
 import re
 
-YT_URL_RE = re.compile(r"""(?x)^
-    (
-        (?:https?://|//)                                    # http(s):// or protocol-independent URL
-        (?:(?:(?:(?:\w+\.)?[yY][oO][uU][tT][uU][bB][eE](?:-nocookie|kids)?\.com|
-        youtube\.googleapis\.com)/                        # the various hostnames, with wildcard subdomains
-        (?:.*?\#/)?                                          # handle anchor (#/) redirect urls
-        (?:                                                  # the various things that can precede the ID:
-            (?:(?:v|embed|e)/(?!videoseries))                # v/ or embed/ or e/
-            |shorts/
-            |(?:                                             # or the v= param in all its forms
-                (?:(?:watch|movie)(?:_popup)?(?:\.php)?/?)?  # preceding watch(_popup|.php) or nothing (like /?v=xxxx)
-                (?:\?|\#!?)                                  # the params delimiter ? or # or #!
-                (?:.*?[&;])??                                # any other preceding param (like /?s=tuff&v=xxxx or ?s=tuff&amp;v=V36LpHqtcDY)
-                v=
-            )
-        ))
-        |(?:
-        youtu\.be|                                        # just youtu.be/xxxx
-        vid\.plus|                                        # or vid.plus/xxxx
-        )/
-        )
-    )?                                                       # all until now is optional -> you can pass the naked ID
-    (?P<id>[0-9A-Za-z_-]{11})                                # here is it! the YouTube video ID
-    (?(1).+)?                                                # if we found the ID, everything can follow
-    $"""
-    )
-
 class CustomYoutubeLoader(YoutubeLoader):
 
     def load(self) -> List[Document]:
@@ -65,10 +38,10 @@ class CustomYoutubeLoader(YoutubeLoader):
     @staticmethod
     def extract_video_id(youtube_url: str) -> str:
         """Extract video id from common YT urls."""
-        match = YT_URL_RE.match(youtube_url)
+        match = re.search(r"(?:youtube\.com/.*v=|youtu\.be/)([^&]+)", youtube_url)
         if not match:
             raise ValueError(f"Could not determine the video ID for the URL {youtube_url}")
-        return match.group("id")
+        return match.group(1)
 
     @classmethod
     def from_youtube_url(cls, youtube_url: str, **kwargs: Any) -> YoutubeLoader:
