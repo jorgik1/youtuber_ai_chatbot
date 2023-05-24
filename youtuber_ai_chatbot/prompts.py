82b48617708195926c10766cea385019ccac77b0
Yuriy Stenin <yuriy.ipcom@gmail.com>
2023-05-24 23:00:51 +0300

    Update prompts.py

--- youtuber_ai_chatbot/prompts.py
+++ youtuber_ai_chatbot/prompts.py
@@ -3,23 +3,21 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate)
 
-
-        # Human question prompt
+# Human question prompt
 human_template = "Answer the following question: {question}"
 HUMAN_PROMPT = HumanMessagePromptTemplate.from_template(human_template)
-        # Template to use for the system message prompt
+
+# Template to use for the system message prompt
 template = """
-  You are a helpful assistant that that can answer questions about youtube videos
-  based on the video's transcript: {docs}
+You are a helpful assistant that can answer questions about YouTube videos based on their transcripts.
 
-  Only use the factual information from the transcript to answer the question.
+To provide accurate answers, please refer to the factual information in the video transcript: {docs}
 
-  If you feel like you don't have enough information to answer the question, say "I don't know".
+If you don't have enough information to answer the question, please respond with "I don't know".
 
-  Your answers should be verbose and detailed.
-  """
+Your answers should be detailed and provide as much information as possible.
+"""
 
 PROMPT_TEMPLATE = SystemMessagePromptTemplate.from_template(template)
-
 
 CHAT_PROMPT = ChatPromptTemplate.from_messages([PROMPT_TEMPLATE, HUMAN_PROMPT])
